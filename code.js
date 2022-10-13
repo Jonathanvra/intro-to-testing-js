6d03e8819de40be349ec28255adb2c787c6d2f9e
Jonathan Vargas <jonatan.vargas3339@gmail.com>
2022-10-13 11:12:54 -0500

    test practice exercise

--- code.js
+++ code.js
@@ -2,3 +2,18 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function subtract(x, y) {
+    if (typeof x === "number" && typeof y === "number") {
+        return x - y;
+    } else {
+        return false;
+    }
+}
+function sayHello(){
+    return "Hello!"
+}
+
+function sayGoodBye(){
+    return "Good Bye!"
+}
--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 // Unit tests for the helloWorld function
 describe('helloWorld', function() {
-    it('should be a defined function', function() {
+    it('should be a defined function within code.js', function() {
       expect(typeof helloWorld).toBe('function');
     });
     it('should return a string when called', function() {
@@ -11,5 +11,46 @@ describe('helloWorld', function() {
     });
     it("should never return 'undefined' when called", function() {
       expect(helloWorld()).not.toBe(undefined);
+    });
+});
+
+describe("subtract", function(){
+    it("should be a defined function",function(){
+        expect(typeof subtract).toBe("function");
+    });
+    it("should return a number when passed two number arguments", function() {
+        expect(typeof subtract(3,1)).toBe("number");
+    });
+    it("should return false when passed any argument that is not a number", function(){
+        expect(subtract()).toBe(false);
+        expect(subtract(2)).toBe(false);
+        expect(subtract("Banana", 6)).toBe(false);
+    });
+    it("should return 34 when passed the arguments of 64 and 30", function(){
+        expect(subtract(64, 30)).toBe(34);
+    });
+    it("should return 34 when passed the arguments of 64 and 30", function(){
+        expect(subtract(100, 25)).toBe(75);
+    });
+    it("should return 34 when passed the arguments of 64 and 30", function(){
+        expect(subtract(0, 7)).toBe(-7);
+    });
+    it("should return 34 when passed the arguments of 64 and 30", function(){
+        expect(subtract(20, -275)).toBe(295);
+    });
+    it("should return 34 when passed the arguments of 64 and 30", function(){
+        expect(subtract(0, 0)).toBe(0);
+    });
+});
+
+describe('sayHello', function(){
+   it("should be a defined function in code.js", function(){
+       expect(typeof sayHello).toBe('function');
+   });
+});
+
+describe('sayGoodBye', function(){
+    it("should be a defined function in code.js", function(){
+        expect(typeof sayGoodBye).toBe('function');
     });
 });
